6c990190d7f1b677e66f71b2d1f375cee56e7d20
Eddie Zaboroskie <ezaboroskie@MacBook-Pro-2.ftsbninc.local>
2023-10-17 16:08:51 -0400

    condenses script.js and 'fixes' last slide issue

--- script.js
+++ script.js
@@ -2,31 +2,24 @@ const backBtn = document.getElementById('enabled-btn-backward')
 const disBackBtn = document.getElementById('disabled-btn-backward')
 const forBtn = document.getElementById('enabled-btn-forward')
 const disForBtn = document.getElementById('disabled-btn-forward')
-const injCont1 = document.getElementById('injection-container1')
-const injCont2 = document.getElementById('injection-container2')
-const injCont3 = document.getElementById('injection-container3')
-const injCont4 = document.getElementById('injection-container4')
-const injCont5 = document.getElementById('injection-container5')
-const injCont6 = document.getElementById('injection-container6')
-const injCont7 = document.getElementById('injection-container7')
-const injCont8 = document.getElementById('injection-container8')
+const injectContainer = document.getElementById('lower-body-container')
+const injectableContent = injectContainer.querySelectorAll(':scope > div')
 
 backBtn.style.display = 'none'
 disForBtn.style.display = 'none'
 
 const swiper = new Swiper('.swiper', {
-
-
+    
     direction: 'horizontal',
     slideToClickedSlide: true,
-    slidesPerView: 2.5,
+    slidesPerView: 'auto',
     spaceBetween: 15,
     
-
     breakpoints:{
         975:{
             slidesPerView: 5,
             spaceBetween:20,
+            slideToClickedSlide: true,
         },
     },
 
@@ -34,22 +27,17 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.enabled-btn-forward',
       prevEl: '.enabled-btn-backward',
     },
-    
-  
   });
-
 
 swiper.on('slideChange', function(){
     const realIndex = swiper.realIndex;
-    const injContainers = [injCont1, injCont2, injCont3, injCont4, injCont5, injCont6, injCont7, injCont8];
-
+    
     backBtn.style.display = (realIndex === 0) ? 'none' : 'flex';
     disBackBtn.style.display = (realIndex === 0) ? 'flex' : 'none';
     disForBtn.style.display = (realIndex === 7) ? 'flex' : 'none';
     forBtn.style.display = (realIndex !== 7) ? 'flex' : 'none';
-
-
-    injContainers.forEach((container, index) => {
+    
+    injectableContent.forEach((container, index) => {
         container.style.display = (index === realIndex) ? 'flex' : 'none';
     });
 });
